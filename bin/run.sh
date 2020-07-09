81ff698ee81eba1e80bb3088576b01a5dd2fc408
dracco1993 <dracco1993@gmail.com>
2020-07-09 19:20:00 -0400

    Auto compile all bots (closes #7)

--- bin/run.sh
+++ bin/run.sh
@@ -2,11 +2,14 @@
 
 # Compile the bots
 # TODO: Remove the hardcoded javac paths from here
-# TODO: Add auto compilation of all .java robot files
-"/mnt/c/Program Files/AdoptOpenJDK/jdk-14.0.1.7-hotspot/bin/javac.exe" -target 8 -source 8 -d ./robots_output/ -cp "C:\robocode\libs\robocode.jar" ./robots_source/Dracco.java
+for f in robots_source/*.java; do
+  echo "Comiling $f"
+  "/mnt/c/Program Files/AdoptOpenJDK/jdk-14.0.1.7-hotspot/bin/javac.exe" -target 8 -source 8 -d ./robots_output/ -cp "C:\robocode\libs\robocode.jar" $f
+done
 
 # Move to the robocode install
 cd /mnt/c/robocode/
 
 # Run robocode
+echo "Running robocode"
 "/mnt/c/Program Files/AdoptOpenJDK/jdk-14.0.1.7-hotspot/bin/java.exe" -Xmx512M -cp libs/robocode.jar -XX:+IgnoreUnrecognizedVMOptions "--add-opens=java.base/sun.net.www.protocol.jar=ALL-UNNAMED" "--add-opens=java.base/java.lang.reflect=ALL-UNNAMED" "--add-opens=java.desktop/javax.swing.text=ALL-UNNAMED" "--add-opens=java.desktop/sun.awt=ALL-UNNAMED" robocode.Robocode
