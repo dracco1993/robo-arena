1596ccaebc1d6fe69ede92a4ea7a664d1b3aa993
dracco1993 <dracco1993@gmail.com>
2020-07-09 19:08:25 -0400

    Working bash and ps scripts that compile and run the game

--- bin/run.ps1
+++ bin/run.ps1
@@ -2,13 +2,15 @@
 $CurrentDir = $(get-location).Path;
 
 # Compile the bots
+# TODO: Remove the hardcoded javac paths from here
+# TODO: Add auto compilation of all .java robot files
 C:\"Program Files"\AdoptOpenJDK\jdk-14.0.1.7-hotspot\bin\javac.exe -target 8 -source 8 -d .\robots_output\ -cp "C:\robocode\libs\robocode.jar" .\robots_source\Dracco.java
 
 # Move to the robocode install
 cd C:\robocode\
 
 # Run robocode
-C:\robocode\robocode.bat
+.\robocode.bat
 
 # Move back to the original directory
 cd $CurrentDir
